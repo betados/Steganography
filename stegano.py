df7748c0351919573c0466348e3b1de827f394bc
wueato03 <alvaro.torres@entelgy.com>
2017-11-16 15:51:33 +0100

    unlimited string length

--- stegano.py
+++ stegano.py
@@ -3,16 +3,17 @@ from PIL import Image
 
 class Stegano(object):
 
-    def encode(self, image, info=12345):
+    def encode(self, image, info='Aguangromanuer una peich ola k ase'):
         im = Image.open(image)
         px = im.load()
         try:
             info = str(info)
         except:
             exit()
+
         for i, char in enumerate(info):
-            binario = bin(int(char))[2:]
-            while len(binario) < 4:
+            binario = bin(ord(char))[2:]
+            while len(binario) < 7:
                 binario = '0' + binario
             for j, bit in enumerate(binario):
                 auxList = [v for v in px[i, j][:3]]
@@ -21,22 +22,22 @@ class Stegano(object):
                 alpha = int(alpha, 2)
                 auxList.append(alpha)
                 px[i, j] = tuple(v for v in auxList)
-                # print(px[i, j])
-                # print('\n')
             im.save('dirty.png')
-
-
 
     def decode(self, image):
         im = Image.open(image)
         px = im.load()
-        numero = ''
-        for i in range(9):
-            binario = ''
-            for j in range(4):
-                binario += bin(px[i, j][3])[-1]
-            numero += str(int(binario,2))
-        return int(numero)
+        info = ''
+        code = ''
+        i = 0
+        while i == 0 or 31 < int(code, 2) < 127:
+            code = ''
+            for j in range(7):
+                code += bin(px[i, j][3])[-1]
+            info += chr(int(code, 2))
+            i += 1
+        return info[:-1]
+
 
 st = Stegano()
 
